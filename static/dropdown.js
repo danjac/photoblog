e91997e1a020a1987d248d3abb833b53fe336912
Dan Jacob <danjac2018@gmail.com>
2026-03-25 09:25:59 +0200

    fix: use

--- static/dropdown.js
+++ static/dropdown.js
@@ -1,16 +1,10 @@
 document.addEventListener('alpine:init', () => {
-  let _nextId = 0;
-
-  Alpine.store('dropdown', { activeId: null });
-
-  document.addEventListener('htmx:beforeRequest', () => {
-    Alpine.store('dropdown').activeId = null;
-  });
-
   Alpine.data('dropdown', () => ({
-    _id: ++_nextId,
-    get open() { return Alpine.store('dropdown').activeId === this._id; },
-    toggle() { Alpine.store('dropdown').activeId = this.open ? null : this._id; },
-    close() { if (this.open) Alpine.store('dropdown').activeId = null; },
+    open: false,
+    toggle() {
+      this.open = !this.open;
+      if (this.open) this.$dispatch('dropdown-open');
+    },
+    close() { this.open = false; },
   }));
 });

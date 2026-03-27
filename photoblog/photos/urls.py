dddc143165b2b2ecd0b4b04f89179449a3ed6da8
Dan Jacob <danjac2018@gmail.com>
2026-03-27 20:01:07 +0200

    refactor: tidy up photo urls

--- photoblog/photos/urls.py
+++ photoblog/photos/urls.py
@@ -9,10 +9,10 @@ if TYPE_CHECKING:
 
 app_name = "photos"
 urlpatterns: list[URLPattern | URLResolver] = [
-    path("photos/", views.photo_list, name="photo_list"),
-    path("photos/users/<str:username>/", views.user_photo_list, name="user_photo_list"),
-    path("photos/create/", views.photo_create, name="photo_create"),
-    path("photos/<int:pk>/", views.photo_detail, name="photo_detail"),
-    path("photos/<int:pk>/edit/", views.photo_edit, name="photo_edit"),
-    path("photos/<int:pk>/delete/", views.photo_delete, name="photo_delete"),
+    path("", views.photo_list, name="photo_list"),
+    path("users/<str:username>/", views.user_photo_list, name="user_photo_list"),
+    path("create/", views.photo_create, name="photo_create"),
+    path("<int:pk>/", views.photo_detail, name="photo_detail"),
+    path("<int:pk>/edit/", views.photo_edit, name="photo_edit"),
+    path("<int:pk>/delete/", views.photo_delete, name="photo_delete"),
 ]

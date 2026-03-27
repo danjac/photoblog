8c00caa56e28a5edf4addbe8426410e5ead37d33
Dan Jacob <danjac2018@gmail.com>
2026-03-28 00:31:08 +0200

    fix: use proper comparison of target in dropdown.js

--- static/dropdown.js
+++ static/dropdown.js
@@ -2,19 +2,22 @@ document.addEventListener('alpine:init', () => {
   Alpine.data('dropdown', () => ({
     open: false,
     init() {
-      window.addEventListener('dropdown-open', this.onDropdownOpen);
-      window.addEventListener('htmx:beforeRequest', this.close);
+      this._dropdownId = this.$id('dropdown');
+      this._onDropdownOpen = this.onDropdownOpen.bind(this);
+      this._close = this.close.bind(this);
+      window.addEventListener('dropdown-open', this._onDropdownOpen);
+      window.addEventListener('htmx:beforeRequest', this._close);
     },
     destroy() {
-      window.removeEventListener('dropdown-open', this.onDropdownOpen);
-      window.removeEventListener('htmx:beforeRequest', this.close);
+      window.removeEventListener('dropdown-open', this._onDropdownOpen);
+      window.removeEventListener('htmx:beforeRequest', this._close);
     },
     onDropdownOpen(e) {
-      if (e.target !== this.$el) this.close();
+      if (e.detail.id !== this._dropdownId) this.close();
     },
     toggle() {
       this.open = !this.open;
-      if (this.open) this.$dispatch('dropdown-open');
+      if (this.open) this.$dispatch('dropdown-open', { id: this._dropdownId });
     },
     close() { this.open = false; },
   }));
